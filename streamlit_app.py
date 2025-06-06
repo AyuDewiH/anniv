5f90540eb822b818344f1682154f39f707a032f8
AyuDewiH <harumidewiayu@gmail.com>
2025-06-06 14:14:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("♥️ a little letter for acil")
 st.write(
     "I hope our relationship can reach the level of marriage☆"
 )
-st.image("1b642d1ee9c2770d57e277c597642480.jpg", width=200)
+st.image("1b642d1ee9c2770d57e277c597642480.jpg", width=350)
