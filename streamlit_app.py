45094d6942abee9b4b3f08eecf2ffbf1458d16b8
AyuDewiH <harumidewiayu@gmail.com>
2025-06-06 14:17:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.write(
     "I hope our relationship can reach the level of marriage☆"
 )
 st.image("1b642d1ee9c2770d57e277c597642480.jpg", width=350)
+st.image("9c4f97f2184c6f0b964cde4103949a05.jpg", width=350)
